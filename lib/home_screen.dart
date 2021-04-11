b216cdf1b25c1f8180b02f4c64feaeb8f96d0d15
Michal Predotka <mivoligo@gmail.com>
2021-04-11 15:36:23 +0100

    Add app bar with title and buttons

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -8,7 +8,32 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<AuthBloc, AuthState>(
       builder: (context, state) {
-        return Scaffold();
+        return Scaffold(
+          body: CustomScrollView(
+            slivers: [
+              SliverAppBar(
+                floating: true,
+                flexibleSpace: FlexibleSpaceBar(
+                  title: Text('Your notes'),
+                ),
+                leading: IconButton(
+                  icon: state is Authenticated
+                      ? Icon(Icons.logout)
+                      : Icon(Icons.account_circle),
+                  onPressed: () => state is Authenticated
+                      ? context.read<AuthBloc>().add(Logout())
+                      : () {},
+                ),
+                actions: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.brightness_4),
+                  ),
+                ],
+              )
+            ],
+          ),
+        );
       },
     );
   }
